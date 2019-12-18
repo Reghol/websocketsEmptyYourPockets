8ca92231eef92a5d2c0ac974e9dd60fc8d993855
Reghol <jakubnc@protonmail.com>
2019-12-18 14:39:11 +0000

    basic set up

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 
 app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/index.html");
+  res.sendFile(__dirname + "/public/index.html");
 });
 
 io.on("connection", function(socket) {
-  console.log("a user connected");
+  socket.on("chat message", function(msg) {
+    io.emit("chat message", msg);
+  });
   socket.on("disconnect", function() {
     console.log("user disconnected");
   });
